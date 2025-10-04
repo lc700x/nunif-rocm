bc1fe99e753ae043301ea06b4fa80aaf2a88893f
lc700x <zjulianxu@gmail.com>
2025-10-04 22:45:31 +0800

    Use genera HIP6 as version

--- InstallerAMD_standalone_legacy.bat
+++ InstallerAMD_standalone_legacy.bat
@@ -2,7 +2,7 @@
 cls
 echo --- IW3 Installer for AMD GPU's on Windows (With ZLUDA)---
 echo.
-echo - Make sure you have installed HIP 6.4.2 and copied your libraries (if you have and older gpu) before installing this. 
+echo - Make sure you have installed HIP 6 and copied your libraries (if you have and older gpu) before installing this. 
 @REM echo - Remember to add "%HIP_PATH%bin" to your PATH in system enviromental variables!!!
 echo.
 echo - Enable Long Path support for torch.compile
@@ -71,14 +71,14 @@ echo.
 @REM echo.
 @REM %PYTHON_EXE% -m iw3.download_models
 echo.
-echo - Patching ZLUDA (Zluda 3.9.5 for HIP SDK 6.4.2)
+echo - Patching ZLUDA (Zluda 3.9.5 for HIP SDK 6)
 %SystemRoot%\system32\curl -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.5e717459179dc272b7d7d23391f0fad66c7459cf/ZLUDA-windows-rocm6-amd64.zip > zluda.zip
 %SystemRoot%\system32\tar -xf zluda.zip
 @REM del zluda.zip
 copy zluda\cublas.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cublas64_11.dll /y >NUL
 copy zluda\cusparse.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cusparse64_11.dll /y >NUL
 copy zluda\nvrtc.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y >NUL
-echo - ZLUDA is patched. (Zluda 3.9.5 for HIP 6.4.2)
+echo - ZLUDA is patched. (Zluda 3.9.5 for HIP 6)
 echo.
 echo You can now use the iw3 gui and cli with gpu acceleration with amd gpu's. 
 echo You can manually put model checkpoint files (*.pth, *.safetensors, .etc) to: nunif-amd\iw3\pretrained_models\hub\checkpoints\
