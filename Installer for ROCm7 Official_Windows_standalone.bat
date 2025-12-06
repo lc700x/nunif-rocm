15bf50ce2f0b79b10d125bb6225666046380ce6f
lc700x <zjulianxu@gmail.com>
2025-12-06 19:47:36 +0800

    Add for RX6000 support

---
+++ Installer for ROCm7 Official_Windows_standalone.bat
@@ -0,0 +1,25 @@
+@echo off
+cls
+echo --- IW3 Installer for AMD GPU's on Windows (With Official ROCm7)---
+
+echo - Setting up the virtual environment
+REM Set paths
+Set "PYTHON_EXE=.\python\python.exe"
+
+echo.
+echo Installing requirements from: %REQUIREMENTS_FILE%
+@REM echo - Installing the requirements from %REQUIREMENTS_FILE%
+echo - Updating pip package
+%PYTHON_EXE% -m pip install --upgrade pip --no-cache-dir --no-warn-script-location --trusted-host http://mirrors.aliyun.com/pypi/simple/
+if %errorlevel% neq 0 (
+  echo Failed to update pip
+  pause
+  exit /b 1
+)
+%PYTHON_EXE% -m pip install -r requirements-rocm7-official.txt --no-cache-dir --no-warn-script-location
+%PYTHON_EXE% -m pip install --no-warn-script-location --no-cache-dir -r requirements.txt --trusted-host http://mirrors.aliyun.com/pypi/simple/
+%PYTHON_EXE% -m pip install --no-warn-script-location --no-cache-dir -r requirements-gui.txt --trusted-host http://mirrors.aliyun.com/pypi/simple/
+@REM %PYTHON_EXE% -m waifu2x.download_models
+@REM %PYTHON_EXE% -m waifu2x.web.webgen
+@REM %PYTHON_EXE% -m iw3.download_models
+pause
