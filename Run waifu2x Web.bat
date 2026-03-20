172ed18e21936c71f485184c3a44d5ec822b0b45
lc700x <zjulianxu@gmail.com>
2026-03-21 00:25:55 +0800

    Fix waifu2x web

--- Run waifu2x Web.bat
+++ Run waifu2x Web.bat
@@ -3,6 +3,6 @@ Set "PYTHON_EXE=.\python\python.exe"
 set "SCRIPT_DIR=%~dp0"
 set "REL_PATH=%SCRIPT_DIR%\python\Scripts"
 set "PATH=%REL_PATH%;%PATH%"
-%PYTHON_EXE% -m waifu2x.web --no-size-limit --cache-ttl 120
+%PYTHON_EXE% -m waifu2x.web
 pause
 exit /b 0
